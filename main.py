d7d0f9aea3643efae812d9b88717c1bb9c5bcec5
nikhil <nikhil.thomas@walamrt.com>
2022-02-03 16:47:27 +0530

    test1

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from pyspark.sql import Row
+from pyspark.sql.functions import col, concat_ws
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
